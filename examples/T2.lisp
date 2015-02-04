f74c29ee28f1339f0bceb78ddfff13643047aea6
Harry Scells <harryscells@gmail.com>
2015-02-04 10:05:40 -0500

    added examples

---
+++ examples/T2.lisp
@@ -0,0 +1,13 @@
+(defmethod make-rule ((play) &aux if-part then-part)
+   (setf if-part (list 'prefix 'of play 'matches 'the 'play 'so 'far))
+   (setf then-part (lst 'select 'move 'from play))
+   (list 'if if-part 'then then-part)
+)
+
+(defmethod matches ((psf list) (play list))
+   (cond
+      ((null psf) t)
+      ((eq (car psf) (car play)) (matches (cdr psf) (cdr play)))
+      (t nil)
+   )
+)
